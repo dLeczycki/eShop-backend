f48ca0b6a33e99bc85a6f22acd868bb34ccd19f6
dLeczycki <danielleczycki@gmail.com>
2022-07-06 12:19:01 +0200

    Add Recipient interfaces

--- types/order.ts
+++ types/order.ts
@@ -1,3 +1,23 @@
+export interface Recipient {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone?: string;
+  postalCode: string;
+  city: string;
+  address: string;
+}
+
+export interface RecipientError {
+  firstname: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  postalCode: string;
+  city: string;
+  address: string;
+}
+
 export interface Shipment {
   name: string;
   fullName: string;
